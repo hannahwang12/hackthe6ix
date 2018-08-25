9966cbf3d0475dd0f15cddbde27113bb8a9ece96
Hannah Wang <hannahwang12@gmail.com>
2018-08-25 00:36:22 -0400

    make login page

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="login">
-          <input id="username" placeholder="username"/>
-          <input id="password" type="password" placeholder="password"/>
+        <h1 className="title">Old People Chatbot</h1>
+        <div className="login"> 
+          <input id="username" classname="loginField" placeholder="username"/>
+          <input id="password" classname="loginField" type="password" placeholder="password"/>
+          <div className="loginButton">login</div>
         </div>
-      </div>
+     
+     
+     
+      </div> 
+  
     );
   }
 }
