a6ababed9b7c4cdf418aa3447cd86380a56979c9
xcjackpan <xcjackpan99@gmail.com>
2018-08-25 02:21:58 -0400

    sign up

--- client/src/components/SignupComponent.js
+++ client/src/components/SignupComponent.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 
-class LoginComponent extends Component {
+class SignUpComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: '',
       password: '',
+      email: '',
     };
     
   }
 
-  loginSubmit = (e) => {
+  signupSubmit = (e) => {
     e.preventDefault();
-    this.props.loginSubmit(this.state.username, this.state.password);
+    this.props.signupSubmit(this.state.username, this.state.password);
+  }
+
+  handleChangeEmail= (e) => {
+    this.setState({email: e.target.value});
   }
 
   handleChangeUser = (e) => {
@@ -26,16 +31,17 @@ class LoginComponent extends Component {
   render() {
     return (
       <div style={{display: this.props.display}}>
-        <h1 className="title">HT6 Chatbot</h1>
+        <h1 className="title">Signup</h1>
         <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe" display="none" frameBorder="0"></iframe>
-        <form id="login" onSubmit={this.loginSubmit} className="login" target="dummyframe" autoComplete="off"> 
+        <form id="login" onSubmit={this.signupSubmit} className="login" target="dummyframe" autoComplete="off">
+          <input id="email" classname="loginField" placeholder="email" value={this.email} onChange={this.handleChangePass}/>
           <input id="username" classname="loginField" placeholder="username" value={this.state.username} onChange={this.handleChangeUser}/>
           <input id="password" classname="loginField" type="password" placeholder="password" value={this.password} onChange={this.handleChangePass}/>
-          <input type="submit" value="Log in!" className="loginButton"/>
+          <input type="submit" value="submit" className="loginButton"/>
         </form>
       </div> 
     );
   }
 }
 
-export default LoginComponent;
+export default SignUpComponent;
