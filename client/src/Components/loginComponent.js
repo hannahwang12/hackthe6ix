c5d4b7d17676acf9f493145fd2bbdbc40f65c40c
Hannah Wang <hannahwang12@gmail.com>
2018-08-26 03:48:54 -0400

    fix text

--- client/src/Components/loginComponent.js
+++ client/src/Components/loginComponent.js
@@ -27,6 +27,7 @@ class LoginComponent extends Component {
     return (
       <div style={{display: this.props.display}} className = "frame">
         <h1 className="title">cl<b>ai</b>re</h1>
+        <p style={{display: this.props.caredisplay, margin: 0, marginBottom: '10px',}}>for families</p>
         <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe_s" display="none" frameBorder="0"></iframe>
         <form id="login" onSubmit={this.loginSubmit} className="login" target="dummyframe_s" autoComplete="off"> 
           <input name="username" className="loginField" placeholder="username" value={this.state.username} onChange={this.handleChangeUser}/>
